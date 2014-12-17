d6f4af921de8115577494bd9f54220937a1a6931
rocknroll420 <doug709@gmail.com>
2014-12-17 13:55:06 -0330

    Generalized for more forms

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ var formidable = require('formidable');
 var util = require('util');
 var mysql = require('mysql');
 
-var db = require('./connection');
+var db = require('./connection'); //db connection info
+var inserts = require('./inserts'); //this file tells the forms how to update the database
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/public/index.html');
@@ -14,34 +15,45 @@ app.get('/', function(req, res){
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.post('/form/welcome', function(req, res){
-	console.log('form/welcome');
+app.post('/form', function(req, res){
+	console.log(util.inspect(req.query));
 	var form = new formidable.IncomingForm();
 	form.encoding = 'utf-8';
 	form.parse(req, function(error, fields, files) {
-		var connection = mysql.createConnection({
-			host: db.host,
-			user: db.user,
-			password: db.password,
-			database: db.db
-		});
-		connection.connect();
-		var post = {
-			module: fields.module,
-			vessel_name: fields.vessel,
-			school_name: fields.school,
-			teacher_name: fields.teacher_name,
-			waterbody: fields.waterBody,
-			weather:fields.weather
-		};
-		connection.query('INSERT INTO trip SET ?', post, function(err, result){
-			if(err) console.log("fucked up query "+ util.inspect(err));
-		});
-		connection.end();
+		if(error){
+			console.log("bad query " + util.inspect(form));
+			res.write(JSON.stringify({error:"Couldn't read form"}));
+			res.end();
+		}
+		else{
+			var connection = mysql.createConnection(db);
+			connection.connect();
+			var post = {};
+			if(inserts[req.query.form]){
+				for(var i in inserts[req.query.form].pairings){
+					post[i] = fields[inserts[req.query.form].pairings[i]];
+				}
+				connection.query('INSERT INTO ' + inserts[req.query.form].table + ' SET ?', post, function(err, result){
+					if(err){
+						console.log("bad query " + util.inspect(post));
+						res.write(JSON.stringify({error:"Post failed"}));
+					}
+					else{
+						console.log(inserts[req.query.form].table + " updated successfully");
+						res.write(JSON.stringify({success:"Database updated"}));
+					}
+					connection.end();
+					res.end();
+				});
+			}
+			else{
+				console.log("Form processing failed, inserts not defined for " + req.query.form);
+				connection.end();
+				res.write(JSON.stringify({error:"Post failed"}));
+				res.end();
+			}
+		}
 	});
-
-	res.write(JSON.stringify({hey:"hey"}));
-	res.end();
 });
 
 http.listen(3000, function(){
--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,100 @@
 (function($){
-	$(document).ready(function(){
-		$.get('forms/welcomeAboard.html', function(data){
-			$('#content').html(data);
-		});
+
+window.onhashchange = function(evt){
+	var hash = evt.target.location.hash.split('/');
+	if(hash.length == 1){
+		updateContent('pages/front.html');
+	}
+	else{
+		var event = new CustomEvent(hash[1], {"detail": hash});
+		document.dispatchEvent(event);
+	}
+};
+
+document.addEventListener('program', function(e){
+	if(e.detail.length == 3){
+		updateContent('pages/' + e.detail[2] + '.html');
+	}
+	else if(e.detail[3] == "form"){
+		var event = new CustomEvent("form", {"detail": e.detail});
+		document.dispatchEvent(event);
+	}
+});
+
+document.addEventListener('form', function(e){
+	makeForm(e.detail[4]);
+});
+
+var updateContent = function(newContent, cb){
+	if(typeof cb == "undefined") cb = function(){return;};
+	$.get(newContent, function(data){
+		$('#content').fadeOut(110, function(){$(this).html(data).fadeIn(110);cb();});
 	});
+};
+
+var makeForm = function(formName){
+	updateContent('forms/'+formName+'.html', function(){
+		var hash = window.location.hash.split("/");
+		$('#back').attr('href', hash[0] + "/" + hash[1] + "/" + hash[2]);
+		updateDateTime();
+		setupValidationHandlers(formName);
+	});
+};
+var setupValidationHandlers = function(formName){
+	$('#'+formName).validate({
+		submitHandler: function(form){
+			var formURL = $('#'+formName).attr('action');
+			var formData = new FormData($('#'+formName)[0]); 
+			console.log(formURL);
+			$.ajax({                                                    
+				url: formURL,
+				data: formData,
+				type: 'POST',
+				contentType: false,                       
+				processData: false,                       
+				success: function (data) {
+					console.log(data);
+					var data = JSON.parse(data);
+					console.log("hi");
+					if(data.success){
+						console.log(data.success);
+						$('#content').fadeOut(110, function(){
+							$(this).html("Data submitted successfully").fadeIn(110).delay(800).fadeOut(110, function(){
+								var hash = window.location.hash.split("/");
+								window.location.hash = hash[0] + "/" + hash[1] + "/" + hash[2];
+							});
+						});
+					}
+					else if(data.error){
+						console.log(data.error);
+					}
+					else{
+						console.log("Unknown error in submitting form");
+					}
+				},
+				error: function (err) {
+					console.log("ERROR getting data. Please try again… ");
+				}
+			});
+		}
+	});
+	$('#' + formName + ' input').keyup(function(){
+		$(this).valid();
+	});
+};
+
+var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+var updateDateTime = function(){
+	var now = new Date();
+	var day = now.getDate();
+	day += " " + months[now.getMonth()];
+	day += " " + now.getFullYear();
+	var time = now.toLocaleTimeString();
+	$('date').html("Date: " + day);
+	$('time').html("Time: " + time);
+};
+
+$(document).ready(function(){
+	updateContent('pages/front.html');
+});
 })(jQuery);
